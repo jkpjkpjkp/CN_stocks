1cf6deed222589c25297caf71df9a2dbb77f2c53
jkp <jiang_kangping@163.com>
2025-10-31 16:15:31 +0800

    argv[1]

---
+++ a.py
@@ -0,0 +1,28 @@
+import polars as pl
+import datetime
+import numpy as np
+
+df = pl.scan_parquet('../data/a_1min.pq').select(
+    'id', 'datetime', 'close'
+)
+
+def a(df, filename):
+    df = df.collect().select(
+        'close',
+        id_hd = pl.col.id + '_' + pl.col.datetime.dt.date().cast(pl.String) + '_' + pl.when(pl.col.datetime.dt.time() < datetime.time(12)).then(pl.lit('a')).otherwise(pl.lit('p')),
+        time = pl.col.datetime.dt.time(),
+    ).sort('id_hd', 'time').select(
+        'id_hd', 'time',
+        ret = pl.col.close / pl.col.close.shift(1).over('id_hd')
+    ).drop_nulls()
+
+    full = df.group_by('id_hd').agg(pl.len()).filter(pl.col.len == 119)
+
+    df = df.filter(pl.col.id_hd.is_in(full['id_hd']))
+
+    data = df['ret'].to_numpy()
+
+    np.save(filename, data)
+
+a(df.filter(pl.col.datetime.dt.date() < datetime.datetime(2024,1,1)), '../data/train.npy')
+a(df.filter(pl.col.datetime.dt.date() >= datetime.datetime(2024,1,1)), '../data/eval.npy')
---
+++ b.py
@@ -0,0 +1,148 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from lightning import LightningModule as Module, LightningDataModule as DataModule
+from lightning.pytorch import Trainer
+from lightning.pytorch.loggers import MLFlowLogger
+from lightning.pytorch.callbacks import RichProgressBar
+from lightning.pytorch.utilities import grad_norm
+import numpy as np
+
+def _precompute_rotary_embeddings(seq_len, head_dim, base=4242, device='cuda'):
+        channel_range = torch.arange(0, head_dim, 2, dtype=torch.float32, device=device)
+        inv_freq = 1.0 / (base ** (channel_range / head_dim))
+        t = torch.arange(seq_len, dtype=torch.float32, device=device)
+        freqs = torch.outer(t, inv_freq)
+        cos, sin = freqs.cos(), freqs.sin()
+        cos, sin = cos.bfloat16(), sin.bfloat16()
+        return cos, sin
+
+def apply_rotary_emb(x, cos, sin):
+    assert x.ndim == 3
+    d = x.shape[-1] // 2
+    x1, x2 = x[..., :d], x[..., d:] # split up last time into two halves
+    y1 = x1 * cos + x2 * sin # rotate pairs of dims
+    y2 = x1 * (-sin) + x2 * cos
+    out = torch.cat([y1, y2], -1) # re-assemble
+    out = out.to(x.dtype) # ensure input/output dtypes match
+    return out
+
+class dm(DataModule):
+    def __init__(self):
+        super().__init__()
+        self.data = np.load('../data/data.npy')[:1000000 * 119]
+    
+    def __len__(self):
+        return len(self.data) // 119
+
+    def __getitem__(self, idx):
+        x = self.data[idx * 119 : (idx+1) * 119]
+        x = torch.tensor(x)
+        x = (torch.clamp(x, 0.99, 1.01) - 1) * 420
+        return x
+
+    def train_dataloader(self):
+        return DataLoader(self, 1024, num_workers=32, shuffle=True)
+
+class tm(Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(1, 128)
+        self.qkv = nn.Linear(128, 3 * 128)
+        self.cos, self.sin = _precompute_rotary_embeddings(118, 128 / 4)
+        self.num_heads = 4
+        device='cuda'
+        channel_range = torch.arange(0, 128, 2, dtype=torch.float32, device=device)
+        inv_freq = 1.0 / (10000 ** (channel_range / 128))
+
+        t = torch.arange(118, dtype=torch.float32, device=device)
+        freqs = torch.outer(t, inv_freq)
+        self.cos, self.sin = freqs.cos().bfloat16(), freqs.sin().bfloat16()
+
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
+
+        self.norm = nn.LayerNorm(128)
+    
+    def rope(self, x):
+        dtype = x.dtype
+        x = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+        x = torch.view_as_real(x * self.freq.unsqueeze(0)).flatten(3)
+        return x.to(dtype)
+    
+    def forward(self, x):
+        b = x.shape[0]
+        assert x.shape == (b, 118)
+
+        x = self.fc1(x.unsqueeze(-1))
+        x = F.silu(x)
+
+        q, k, v = torch.chunk(self.qkv(x), 3, -1)
+        q = apply_rotary_emb(q, self.cos, self.sin)
+        k = apply_rotary_emb(k, self.cos, self.sin)
+
+        shape = (b, 118, 32, 4)
+        q = q.view(*shape)
+        k = k.view(*shape)
+        v = v.view(*shape)
+
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        y = y.transpose(1, 2).contiguous().view(b, 118, 128)
+        y = self.fc2(y)
+        y = F.silu(y)
+
+        x = y
+        y = self.norm(y)
+        q, k, v = torch.chunk(self.qkv(x), 3, -1)
+        q = apply_rotary_emb(q, self.cos, self.sin)
+        k = apply_rotary_emb(k, self.cos, self.sin)
+
+        shape = (b, 118, 32, 4)
+        q = q.view(*shape)
+        k = k.view(*shape)
+        v = v.view(*shape)
+
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        y = y.transpose(1, 2).contiguous().view(b, 118, 128)
+        y = self.fc2(y)
+        y = F.silu(y)
+
+        y = self.fc3(x + y).squeeze(-1)
+
+        return y
+    
+    def training_step(self, batch, batch_idx):
+        y = self(batch[:, :-1])
+        loss = nn.HuberLoss()(batch[:, 1:] * 3, y * 3)
+        if batch_idx % 10 == 0:
+            self.log('train/loss', loss, prog_bar=True, on_epoch=True, on_step=True, logger=True)
+        return loss
+        
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=3e-4)
+
+    def on_before_optimizer_step(self, optimizer):
+        layer_norms = grad_norm(self, norm_type=2)
+        self.log_dict(layer_norms, on_step=True, on_epoch=True, logger=True)
+    
+    def on_train_epoch_end(self):
+        weight_norms = {}
+        for name, param in self.named_parameters():
+            if 'weight' in name and param.requires_grad:  # Focus on weight tensors
+                norm = torch.norm(param).item()  # L2 norm (default); use p=1 for L1
+                weight_norms[f"{name}_weight_norm"] = norm
+        
+        self.log_dict(weight_norms, on_epoch=True, logger=True)
+
+
+if __name__ == '__main__':
+    torch.set_float32_matmul_precision('medium')
+    trainer = Trainer(
+        max_epochs=42,
+        gradient_clip_val=1.,
+        callbacks=[RichProgressBar()],
+        logger=MLFlowLogger(experiment_name="lightning_logs", tracking_uri="file:./ml-runs"),
+    )
+
+    trainer.fit(tm(), dm())
